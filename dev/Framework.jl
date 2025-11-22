ed0ef5c3f62a34f5ac82abd79d7cee7ac2e8add3
Dongdong Kong <kongdd@users.noreply.github.com>
2025-11-22 09:23:15 +0800

    add batchsize

---
+++ dev/Framework.jl
@@ -0,0 +1,65 @@
+using Random, Lux, Optimisers, ComponentArrays, Statistics, Printf
+using MLUtils  # DataLoader
+using Enzyme
+
+
+of_MSE(obs, sim) = mean(abs2, sim .- obs)
+
+function of_NSE(obs, sim)
+  top = sum((sim .- obs) .^ 2)
+  bot = sum((obs .- mean(obs)) .^ 2)
+  return 1 - (top / bot)
+end
+
+function loss_function(model, ps, st, X, Y; predict::Function)
+  h_pred, = predict(model, ps, st, X, Y[:, 1])
+  return -of_NSE(Y, h_pred) # of_MSE(Y, h_pred)
+end
+
+
+function train(X, Y, model; predict::Function, 
+  lr=0.0005, nepoch=1000, step=50, batchsize=-1)
+
+  batchsize > 0 && (loader_train = DataLoader((; X, Y); batchsize, shuffle=true))
+
+  rng = Random.Xoshiro(1) # Use a seeded local RNG for reproducibility
+  ps, st = Lux.setup(rng, model)
+  ps_c = ComponentArray(ps) # 参数扁平化
+  dps = Enzyme.make_zero(ps_c) # 梯度缓存
+
+  opt = Optimisers.ADAM(lr)
+  opt_state = Optimisers.setup(opt, ps_c)
+
+  println("Start Training: Layers=$(L_LAYERS), Steps=$(size(forcing, 2))")
+  println("-"^30)
+  compute_loss(p, X, Y) = loss_function(model, p, st, X, Y; predict)
+
+  function grads!(dps, xb, yb)
+    dps .= 0
+    Enzyme.autodiff(
+      Enzyme.set_runtime_activity(Reverse),
+      compute_loss, Active,
+      Duplicated(ps_c, dps), Const(xb), Const(yb)
+    )
+  end
+
+  for epoch in 1:nepoch
+    if batchsize > 0
+      for (xb, yb) in loader_train
+        grads!(dps, xb, yb) # 计算梯度
+        opt_state, ps_c = Optimisers.update(opt_state, ps_c, dps) # 参数更新
+      end
+    else
+      grads!(dps, X, Y) # 计算梯度
+      opt_state, ps_c = Optimisers.update(opt_state, ps_c, dps) # 参数更新
+    end
+
+    if epoch % step == 0
+      loss = compute_loss(ps_c, forcing, θ_obs)
+      @printf "Epoch %3d | Loss: %.6f\n" epoch loss
+    end
+  end
+
+  ypred, st_new = predict(model, ps_c, st, forcing, θ_obs[:, 1])
+  return ypred, model, ps_c
+end
--- dev/SM_02_Enzyme.jl
+++ dev/SM_02_Enzyme.jl
@@ -1,47 +1,38 @@
-# 这里带入了真实观测值
+# 带入了真实观测值
 # kongdd, 20251121
 using Lux, Random, Optimisers, ComponentArrays, Statistics, Printf
+using MLUtils  # DataLoader
 using Enzyme
 using RTableTools
+include("Framework.jl")
 
-function of_NSE(obs, sim)
-  top = sum((sim .- obs) .^ 2)
-  bot = sum((obs .- mean(obs)) .^ 2)
-  return 1 - (top / bot)
-end
-
-## 带入真实观测
 f = "data/SM_AR_Batesville_8_WNW_2024.csv"
 df = fread(f)
 
+# [n_layer, ntime], depth: 5, 10, 20, 50, 100 cm
+θ_obs = Matrix(df[:, 3:end])' |> collect .|> Float32
 forcing = Matrix(df[:, [:P_CALC]])' |> collect .|> Float32 # [1, ntime]
-θ_obs = Matrix(df[:, 3:end])' |> collect .|> Float32 # [n_layer, ntime], depth: 5, 10, 20, 50, 100 cm
-
-# 1. 配置与数据生成 (Configuration & Data)
-const L_LAYERS = 5      # 土壤层数
-# const BATCH_SZ = 32      # 批次大小
-# const D_INPUT = 2        # 驱动变量: [降雨, 蒸发]
-
+X, Y = forcing, θ_obs
 
 # 定义网络拟合变化量: Δh = Net(h_prev, u_curr)
-function build_network(; n_layers=5, n_in=1)
+function build_network(; n_layers=5, n_in=1, scale=0.01f0)
   in_dim = n_layers + n_in
   return Chain(
     Dense(in_dim => 64, tanh),
     Dense(64 => 64, tanh),
     Dense(64 => n_layers),  # 输出每层的含水量变化 Δh
-    x -> x .* 0.01f0        # 缩放输出，防止初始梯度爆炸
+    x -> x .* scale       # 缩放输出，防止初始梯度爆炸
   )
 end
 
-# 3. 前向传播与时间步进 (Forward Pass)
 # 显式展开时间循环 (Unroll)，模拟物理递推
-function time_step_forward(model, ps, st, forcing, θ_init)
+function predict(model, ps, st, forcing, θ_init)
   # u_seq: [In, T, B], h_init: [L, B]
   T = eltype(θ_init)
   ntime = size(forcing, 2)
+  nlayer = length(θ_init)
   # h_preds = Array{T}(undef, L_LAYERS, ntime, BATCH_SZ)
-  h_preds = Array{T}(undef, L_LAYERS, ntime)
+  h_preds = Array{T}(undef, nlayer, ntime)
   h_preds[:, 1] .= θ_init
   st_curr = st
 
@@ -58,75 +49,28 @@ function time_step_forward(model, ps, st, forcing, θ_init)
 end
 
 
-# predict(model, ps, st, forcing, θ_obs[:, 1])
-function predict(model, ps, st, forcing, θ_init)
-  h_pred, _ = time_step_forward(model, ps, st, forcing, θ_init)
-  return h_pred
-end
-
-# 4. 训练核心 (Training Core)
-function loss_function(model, ps, st, forcing, θ_obs)
-  # 使用数据的 t=1 时刻作为初始条件
-  h_init = @view θ_obs[:, 1]
-  (h_pred, st_new) = time_step_forward(model, ps, st, forcing, h_init) # 预测整个序列
-  return -of_NSE(θ_obs, h_pred)
-  # loss = mean(abs2, h_pred .- θ_obs) # 计算 MSE Loss
-  # return loss
-end
-
-
-function main(; lr=0.0005, nepoch=1000, step=50)
-  rng = Random.Xoshiro(1) # Use a seeded local RNG for reproducibility
-
-  # 初始化
-  model = build_network()
-  ps, st = Lux.setup(rng, model)
-  ps_c = ComponentArray(ps) # 参数扁平化
-  dps = zero(ps_c) # 梯度缓存
-
-  opt = Optimisers.ADAM(lr)
-  opt_state = Optimisers.setup(opt, ps_c)
-
-  # 数据
-  println("Start Training: Layers=$(L_LAYERS), Steps=$(size(forcing, 2))")
-  println("-"^30)
-
-  # 包装函数，仅返回 loss 标量
-  # compute_loss(p, model, state, u, h) = loss_function(model, p, state, u, h)
-  compute_loss(p) = loss_function(model, p, st, forcing, θ_obs)
-
-  for epoch in 1:nepoch
-    dps .= 0
-    # 使用 set_runtime_activity(Reverse) 解决 "Constant memory is stored ... to a differentiable variable" 错误
-    Enzyme.autodiff(
-      Enzyme.set_runtime_activity(Reverse), 
-      # Reverse, 
-      compute_loss, Active,
-      Duplicated(ps_c, dps),
-    )
-
-    opt_state, ps_c = Optimisers.update(opt_state, ps_c, dps) # 参数更新
-    if epoch % step == 0
-      loss = compute_loss(ps_c)
-      @printf "Epoch %3d | Loss: %.6f\n" epoch loss
-    end
-  end
-
-  ypred = predict(model, ps_c, st, forcing, θ_obs[:, 1])
-  return ypred
-end
-
-@time ypred = main(; lr=0.005, nepoch=5000, step=100)
+model = build_network(; scale=0.001f0)
+@time ypred = train(X, Y, model; predict, lr=0.002, 
+  nepoch=2000, step=50, batchsize=-1)[1] # 不划分batchsize, 需要训练很多次
 # 1.893517 seconds (4.55 M allocations: 4.220 GiB, 20.33% gc time)
 
+# @time ypred = train(data...; lr=0.002, nepoch=1000, step=100,
+#   scale=0.001f0, batchsize=24*30)[1] # 采用batchsize训练不成功
+
 begin
+  rng = Random.Xoshiro(1) # Use a seeded local RNG for reproducibility
+  model = build_network(; scale=0.01f0)
+  ps, st = Lux.setup(rng, model) #
+end
+
+if true
   using Plots
-  function plot_layer(i) 
+  function plot_layer(i)
     plot(θ_obs[i, :], label="Observed", xlabel="Layer $i", ylabel="Soil Moisture")
     plot!(ypred[i, :], label="Predicted")
   end
 
   ps = map(plot_layer, 1:5)
-  p = plot(ps..., layout=(3,2), size=(800,600))
+  p = plot(ps..., layout=(3, 2), size=(800, 600))
   savefig("Figure02_SM_Enzyme.png")
 end
